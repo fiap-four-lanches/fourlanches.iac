83deea2f784e0c302e53da000f605aa56d5f71d7
Felipe <eu@felipemagalha.es>
2023-11-02 16:48:41 +0100

    fix: added default value for variables

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "location" {
   description = "Name of the location where the resources will be provisioned"
   type        = string
+  default     = "eastus"
 }
 
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
+  default     = "FourLanches-RG"
 }
 
 variable "project_name" {
   description = "Name of the project"
   type        = string
+  default     = "fourlanches"
 }
