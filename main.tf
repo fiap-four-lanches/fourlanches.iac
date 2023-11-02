4bfab4b62512fe0b65616f89673821b2046945c2
Felipe <eu@felipemagalha.es>
2023-11-02 16:37:16 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+module "functions" {
+  source         = "./modules/functions"
+  resource_group = azurerm_resource_group.main
+  project_name   = var.project_name
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "location" {
+  description = "Name of the location where the resources will be provisioned"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
