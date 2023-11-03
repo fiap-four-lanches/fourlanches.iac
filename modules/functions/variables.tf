2c5147e9479f3b73f7c80aa51dab348a3c4a24ba
Felipe Magalhães <32075171+felipemag@users.noreply.github.com>
2023-11-03 02:01:25 +0100

    fix: Add state (#3)
    * fix: add state
    
    * fix: format

--- modules/functions/variables.tf
+++ modules/functions/variables.tf
@@ -2,6 +2,10 @@ variable "resource_group" {
   description = "The name of the Azure Resource Group"
 }
 
+variable "storage_account" {
+  description = "The name of the main Storage Account"
+}
+
 variable "project_name" {
   description = "Name of the project"
   type = string
