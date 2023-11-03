8004b920727325b17496bebc66cf200712292483
Felipe <eu@felipemagalha.es>
2023-11-03 01:51:47 +0100

    fix: add state

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 3.0.2"
     }
   }
+  backend "azurerm" {
+    resource_group_name   = "eastus-fourlanches-rg"
+    storage_account_name  = "eastusfourlanchessa"
+    container_name        = "terraform"
+    key                   = "terraform.tfstate"
+  }
 
   required_version = ">= 1.1.0"
 }
@@ -18,10 +24,19 @@ resource "azurerm_resource_group" "main" {
   location = var.location
 }
 
+resource "azurerm_storage_account" "main_sa" {
+  name                     =  "${var.location}${var.project_name}sa"
+  resource_group_name        = azurerm_resource_group.main.name
+  location                   = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
 module "functions" {
   source         = "./modules/functions"
   resource_group = azurerm_resource_group.main
   project_name   = var.project_name
+  storage_account = azurerm_storage_account.main_sa
 }
 
 module "dbs" {
--- modules/functions/main.tf
+++ modules/functions/main.tf
@@ -1,11 +1,3 @@
-resource "azurerm_storage_account" "main_sa" {
-  name                     =  "${var.resource_group.location}${var.project_name}sa"
-  resource_group_name        = var.resource_group.name
-  location                   = var.resource_group.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
 resource "azurerm_service_plan" "main_app_service" {
   name                     = "${var.resource_group.location}-${var.project_name}-app-service"
   resource_group_name        = var.resource_group.name
@@ -18,8 +10,8 @@ resource "azurerm_linux_function_app" "auth_function" {
   name                     = "${var.resource_group.location}-${var.project_name}-auth-function"
   resource_group_name        = var.resource_group.name
   location                   = var.resource_group.location
-  storage_account_name       = azurerm_storage_account.main_sa.name
-  storage_account_access_key = azurerm_storage_account.main_sa.primary_access_key
+  storage_account_name       = var.storage_account.name
+  storage_account_access_key = var.storage_account.primary_access_key
   service_plan_id            = azurerm_service_plan.main_app_service.id
   site_config {}
 }
