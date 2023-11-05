acbdce398f64da9569c9e8f6c6325999eba6cf76
Felipe <eu@felipemagalha.es>
2023-11-05 21:11:46 +0100

    fix: upgrade terraform and fix functions

--- modules/functions/main.tf
+++ modules/functions/main.tf
@@ -5,8 +5,14 @@ resource "azurerm_linux_function_app" "auth_function" {
   storage_account_name       = var.storage_account.name
   storage_account_access_key = var.storage_account.primary_access_key
   service_plan_id            = var.service_plan.id
-  site_config {}
+  site_config {
+    application_stack {
+      java_version = "17"
+    }
+  }
+
   app_settings = {
+    FUNCTIONS_WORKER_RUNTIME = "java"
     "DB_LOGIN": var.db_login
     "DB_PASSWORD": var.db_password
     "DB_HOST": var.db_host
